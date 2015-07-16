7cf4c03295dee8c8842f8209df3a023ef7d5cf84
windowsazure <windowsazure>
2015-07-16 04:02:28 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-6-3
+ * Time: 下午10:09
+ */
+
+
+$url = "http://nitrd1.sinaapp.com/mybeacon.php";
+echo "<script language='javascript' type='text/javascript'>";
+
+echo "</script>";
+echo "hello";
+
+
+?>
