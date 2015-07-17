278c9d6937ea11a2931ae9a85438615c14c9dc38
jimiao <Passw0rd>
2015-07-17 12:56:18 +0900

    update

--- rdupdate_sse.php
+++ rdupdate_sse.php
@@ -24,8 +24,6 @@ $sql = $sql."AND M.major = B.major AND M.minor = B.minor GROUP BY U.uuid ";
 
 $result = mysql_query($sql, $conn);
 while ($row=mysql_fetch_row($result)) {
-    $arrayReturn=array('username'=>$row[0],'locationname'=>$row[1],'status'=>$row[2]);
-    echo $arrayReturn;
     $d = array("locationname"=>$row[1],"username"=>$row[0],"status"=>$row[2]);
     echo "data:".json_encode($d)."\n\n";
 
