dcd6203b4b34eb3838572c80141167251ee8249c
jimiao <Passw0rd>
2015-11-02 19:20:27 +0900

    20150825 新サイト移行

--- rdroom.php
+++ rdroom.php
@@ -52,9 +52,9 @@ echo "<caption style='font-size:15px' align='left'>ルーム数：<label id='tab
 echo "<table id='Table' border=1 cellpadding=10 cellspacing=2 bordercolor=#ffaaoo padding='0px'>";
 
 //表头
-$thstr = "<th width='100'>" . implode("</th><th width='100'>", $dbcolarray) . " </th>";
+$thstr = "<th width='150'>" . implode("</th><th width='100'>", $dbcolarray) . " </th>";
 echo $thstr;
-echo "<th width='120'><input type='button' value='Add' onclick='addFun()' /> </th>";
+echo "<th width='150'><input type='button' value='Add' onclick='addFun()' /> </th>";
 
 //表中的内容
 while ($row=mysql_fetch_array($result, MYSQL_ASSOC))//与$row=mysql_fetch_assoc($result)等价
