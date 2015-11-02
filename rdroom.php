7213f9b99eaad37c167cf6c7b1c803e2760e1006
jimiao <Passw0rd>
2015-11-02 16:49:08 +0900

    20150825 新サイト移行

--- rdroom.php
+++ rdroom.php
@@ -61,8 +61,15 @@ while ($row=mysql_fetch_array($result, MYSQL_ASSOC))//与$row=mysql_fetch_assoc(
 {
     echo "<tr>";
     $tdstr = "";
+    $index = 0;
     foreach ($dbcolarray as $td)
-        $tdstr .= "<td>$row[$td]</td>";
+        if (index == 3){
+            $tdstr .= "<td><input type='checkbox' disabled='true' value=$row[$td]></td>";
+        }else{
+            $tdstr .= "<td>$row[$td]</td>";
+        }
+
+        $index = $index+1;
     echo $tdstr;
 
     echo "<td>";
@@ -88,7 +95,7 @@ mysql_close($conn);
         <td><input  style="width:20px"  type=text id="editdiv_id" size="1" readonly="true" /></td>
         <td><input type=text id="editdiv_roomid" size="15"/></td>
         <td><input type=text id="editdiv_roomname" size="45"/></td>
-        <td><input type=text id="editdiv_visible" size="45"/></td>
+        <td><input type=checkbox id="editdiv_visible" size="45"/></td>
         <td><input type=button name="Update" value="Update" onclick="updateFunRoom()" /></td>
     </tr>
 </table>
@@ -100,7 +107,7 @@ mysql_close($conn);
             <td class="cellid">Auto</td>
             <td><input type=text id="adddiv_roomid" size="15"/></td>
             <td><input type=text id="adddiv_roomname" size="45"/></td>
-            <td><input type=text id="adddiv_visible" size="45"/></td>
+            <td><input type=checkbox id="adddiv_visible" size="45"/></td>
             <td><input type=button name="Insert" value="Insert" onclick="insertFunRoom()" /></td>
         </tr>
     </table>
