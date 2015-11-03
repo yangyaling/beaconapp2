70912a418c7f8049f662e7b942247854d3583872
jimiao <Passw0rd>
2015-11-03 12:53:09 +0900

    20150825 新サイト移行

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ while ($row=mysql_fetch_array($result, MYSQL_ASSOC)) {
     echo "</tr>";
 }
 echo "</table>";
-echo "<br/><a href='login.html'>■管理画面へ</a>";
+//echo "<br/><a href='login.html'>■管理画面へ</a>";
 //echo "<br/><a href='mybeacon.php'>■Beacon管理</a>";
 echo "</div>";
 
