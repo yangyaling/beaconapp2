849c555bda3e253f6bd35aae450d3361950f3278
jimiao <Passw0rd>
2016-04-05 14:01:16 +0800

    20160404

--- rduser.php
+++ rduser.php
@@ -88,11 +88,11 @@ mysql_close($conn);
 <table id="editItem" border=1 cellpadding=10 cellspacing=2 bordercolor=#ffaaoo padding='0px'>
     <th>id</th><th>userid</th><th>username</th><th>visible</th><th>listindex</th><th>Action</th>
     <tr>
-        <td><input size="3" type=text id="editdiv_id" size="1" readonly="true" /></td>
-        <td><input size="3" type=text id="editdiv_userid"  readonly="true"/></td>
-        <td><input size="15" type=text id="editdiv_username"/></td>
-        <td><input size="3" type=text id="editdiv_visible"/></td>
-        <td><input size="3" type=text id="editdiv_listindex"/></td>
+        <td><input type=text id="editdiv_id" size="1" readonly="true" /></td>
+        <td><input  type=text id="editdiv_userid"  readonly="true"/></td>
+        <td><input type=text id="editdiv_username"/></td>
+        <td><input type=text id="editdiv_visible"/></td>
+        <td><input type=text id="editdiv_listindex"/></td>
         <td><input type=button name="Update" value="Update" onclick="updateFunUser()" /></td>
     </tr>
 </table>
