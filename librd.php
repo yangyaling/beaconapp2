874e7952ca39adbd1f19d1be5e2a8999cedd918f
jimiao <jimiao@hz.totyu.com>
2017-07-06 10:01:26 +0800

    adsa

--- librd.php
+++ librd.php
@@ -14,7 +14,7 @@
 define("SAE_MYSQL_HOST_M", "beacon.mysqldb.chinacloudapi.cn");
 define("SAE_MYSQL_USER", "beacon%yangyl");
 define("SAE_MYSQL_PASS", "Passw0rd");
-define("SAE_MYSQL_DB", "beacon/beacondb");
+define("SAE_MYSQL_DB", "beacondb");
 
 function getmonitorinfo($useruuid)
 {
