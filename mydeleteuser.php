608814d1f2b6129d5b1c7ec1794c7d0d6a8e1700
jimiao <jimiao@hz.totyu.com>
2017-07-07 11:21:22 +0800

    adsa

--- mydeleteuser.php
+++ mydeleteuser.php
@@ -6,11 +6,13 @@ include 'lib.php';
 $id = $GET['id'];
 
 //delete row in db
-
-
+$errors = array();
+$code = '200';
 if ($conn) {
     $sql = sprintf("delete from %s where id=%d", 'RDUSERINFO', $id);
-    $result = query_sql($sql, $conn);
+    $result = query_sql($sql, $conn, $code, $errors);
+
+    echo $code . "<br/>" . print_r($errors);
 
     if ($result)
 
