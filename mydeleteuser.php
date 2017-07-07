72a1e7b4cfd9bf20c495646312bc155c5bf6eef8
jimiao <jimiao@hz.totyu.com>
2017-07-07 11:28:55 +0800

    adsa

--- mydeleteuser.php
+++ mydeleteuser.php
@@ -3,24 +3,22 @@
 include 'lib.php';
 //params
 
-$id = $POST['id'];
+$id = $_POST['id'];
 
 //delete row in db
-$errors = array();
-$code = '200';
-if ($conn) {
-    $sql = sprintf("delete from %s where id=%d", 'RDUSERINFO', $id);
-//    $result = query_sql($sql, $conn, $code, $errors);
-    $result = mysql_query($sql, $conn);
-    mysql_close($conn);
-    if ($result)
 
-        echo "t";
+$sql = sprintf("delete from %s where id=%d", 'RDUSERINFO', $id);
 
-    else
+$result = mysql_query($sql, $conn);
 
-        echo "f";
-}
-//closeConnection($conn);
+mysql_close($conn);
+
+if ($result)
+
+    echo "t";
+
+else
+
+    echo "f";
 
 ?>
