1cecdea65d27de16f448fdf388c7f06f013a39c6
jimiao <jimiao@hz.totyu.com>
2017-07-07 13:22:39 +0800

    adsa

--- rdgetalluserinfo.php
+++ rdgetalluserinfo.php
@@ -6,9 +6,37 @@
  * Time: 11:55
  */
 
-include 'librd.php';
+include 'lib.php';
 
-$arrReturn = getalluserinfo();
-sendResponse(json_encode($arrReturn));
+//多条数据需要遍历
+$arrayReturn = array();
+if ($conn) {
+
+    $str1 = " SELECT U.uuid, U.username, if(IsNull( B.locationname ),'',B.locationname )AS locationname, ";
+    $str2 = " if(IsNull( M.updatetime ),'',M.updatetime ) AS updatetime, count( M.updatetime ) ,U.status2  ";
+    $str3 = " FROM RDUSERINFO U LEFT JOIN ( ";
+    $str4 = " SELECT M . * ";
+    $str5 = " FROM RDUSERSTATUS M ";
+    $str6 = " WHERE M.updatetime = ( ";
+    $str7 = " SELECT MAX( MM.updatetime ) ";
+    $str8 = " FROM RDUSERSTATUS MM ";
+    $str9 = " WHERE M.useruuid = MM.useruuid ) ";
+    $str10 = " GROUP BY M.useruuid ";
+    $str11 = " ) AS M ON U.uuid = M.useruuid ";
+    $str12 = " LEFT JOIN RDBEACONINFO B ON M.uuid = B.uuid ";
+    $str13 = " AND M.major = B.major ";
+    $str14 = " AND M.minor = B.minor ";
+    $str15 = " GROUP BY U.uuid ";
+
+    $strquery = $str1 . $str2 . $str3 . $str4 . $str5 . $str6 . $str7 . $str8 . $str9 . $str10 . $str11 . $str12 . $str13 . $str14 . $str15;
+    $result = query_sql($strquery);
+    $index = 0;
+    while ($row = mysql_fetch_row($result)) {
+        $arrayReturn[$row[0]] = array('username' => $row[1], 'locationname' => $row[2], 'lasttime' => $row[3], 'status' => $row[4], 'status2' => $row[5]);
+    }
+}
+
+//$arrReturn = getalluserinfo();
+sendResponse(json_encode($arrayReturn));
 
 ?>
--- rdgetmonitorinfo.php
+++ rdgetmonitorinfo.php
@@ -6,9 +6,21 @@
  * Time: 11:55
  */
 
-include 'librd.php';
+include 'lib.php';
 $uuid = $_GET["uuid"];
-$arrReturn = getmonitorinfo($uuid);
-sendResponse(json_encode($arrReturn));
+$arrayReturn = array();
+
+if ($conn) {
+    $strquery = "SELECT U.username,B.locationname,M.updatetime,M.status FROM RDMONITOR M LEFT JOIN RDUSERINFO U ON M.useruuid = U.uuid LEFT JOIN RDBEACONINFO B ON M.uuid=B.UUID AND M.major=B.major AND M.minor = B.minor   WHERE M.useruuid ='" . $useruuid . "' ORDER BY M.updatetime desc LIMIT 10";
+    $result = query_sql($strquery);
+    $index = 0;
+    while ($row = mysql_fetch_row($result)) {
+        $index = $index + 1;
+        $arrayReturn[$row[0] . $row[2]] = array('location' => $row[1], 'updatetime' => $row[2], 'status' => $row[3]);
+    }
+}
+
+//$arrReturn = getmonitorinfo($uuid);
+sendResponse(json_encode($arrayReturn));
 
 ?>
