254e36afe58edfc91e09b28715d41feb7906c584
jimiao <jimiao@hz.totyu.com>
2017-07-10 09:13:59 +0800

    adsa

--- login.php
+++ login.php
@@ -17,7 +17,6 @@ $password = $posts["password"];
 $username = $posts["username"];
 
 //查询用户是否存在
-//$result=mysql_query("SELECT * FROM user where userid='$username' and password='$password'",$conn);
 $sql = sprintf("SELECT * FROM user where userid='%s' and password='%s'",$username,$password);
 $result = query_sql($sql, $conn, $code, $errors);
 
