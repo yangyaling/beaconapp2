8621a167c13e0c5075416d98c22726e34fb57521
jimiao <jimiao@hz.totyu.com>
2017-07-10 16:13:55 +0800

    adsa

--- lib.php
+++ lib.php
@@ -7,49 +7,51 @@
  */
 
 // MySQL服务器
-define("SAE_MYSQL_HOST_M", "beacon.mysqldb.chinacloudapi.cn");
-define("SAE_MYSQL_USER", "beacon%yangyl");
+define("SAE_MYSQL_HOST_M", "rdbeacon.database.chinacloudapi.cn");
+define("SAE_MYSQL_USER", "yangyl");
 define("SAE_MYSQL_PASS", "Passw0rd");
-define("SAE_MYSQL_DB", "beacondb");
+define("SAE_MYSQL_DB", "rdbeacondb");
 
-//打开非持久的 MySQL 连接
-$conn = @mysql_connect(SAE_MYSQL_HOST_M . ':' . SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS) or die("Could not connect:" . mysql_error());
-
-// 设置活动的 MySQL 数据库
-mysql_select_db(SAE_MYSQL_DB, $conn);
-
+// 连接到数据库
+function openConnection()
+{
+    $connectionOptions = array(
+        'Database' => SAE_MYSQL_DB,
+        'Uid' => SAE_MYSQL_USER,
+        'PWD' => SAE_MYSQL_PASS,
+        'CharacterSet' => 'UTF-8'
+    );
+    return sqlsrv_connect("tcp:" . SAE_MYSQL_HOST_M . ",1433", $connectionOptions);
+}
 
 // 关闭连接
 function closeConnection($conn)
 {
-    // mysql数据库
-    mysql_close($conn);
 
     // sqldatabase
-    //    sqlsrv_close($conn);
+    sqlsrv_close($conn);
 }
 
-// 连接到数据库
+// 执行查询处理
 function query_sql($sql, $conn, &$code, &$errors)
 {
-    $result = mysql_query($sql, $conn);
-//    $result = sqlsrv_query($conn, $sql);
+    $result = sqlsrv_query($sql, $conn);
 
     if (!$result) {
         $code = '501';
 
-        $errors = mysql_error();
-        //  $errors = sqlsrv_errors();
+        $errors = sqlsrv_errors();
     }
     return $result;
 }
 
 // 从结果集中取得一行作为关联数组，或数字数组，或二者兼有
-function fetch_single_row($result, $result_type = MYSQL_BOTH)
+function fetch_single_row($result)
 {
-    if ($result_type != null) {
-        return mysql_fetch_array($result, $result_type);
-    } else {
-        return mysql_fetch_row($result);
-    }
+    return sqlsrv_fetch_array($result);
+}
+
+$conn = openConnection();
+if (!$conn) {
+    die("Could not connect:" . sqlsrv_errors());
 }
