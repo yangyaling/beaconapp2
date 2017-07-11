6741c54c61f8b75a9d0ed6baf5583bd7bf0bfc75
jimiao <jimiao@hz.totyu.com>
2017-07-11 09:45:11 +0800

    adsa

--- myinsertroom.php
+++ myinsertroom.php
@@ -6,12 +6,13 @@ $roomid = $_POST['roomid'];
 $roomname = $_POST['roomname'];
 $visible = $_POST['visible'];
 
-$sql = sprintf("INSERT INTO %s(id,roomid,roomname,visible) VALUES(null,'%s', '%s', '%s')", 'RDROOM',$roomid, $roomname,$visible);
+$sql = sprintf("INSERT INTO %s(roomid,roomname,visible) VALUES('%s', '%s', '%s')", 'RDROOM',$roomid, $roomname,$visible);
 $result = query_sql($sql, $conn, $code, $errors);
 
 closeConnection($conn);
 if ($result)
-    echo mysql_insert_id($conn);
+    //echo mysql_insert_id($conn);
+    echo "t";
 else
     echo 'f';
 
