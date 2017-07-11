013f0d670bba5d0036ba1898f426ed00b260af66
jimiao <jimiao@hz.totyu.com>
2017-07-11 15:23:43 +0800

    adsa

--- rdgetmonitorinfo.php
+++ rdgetmonitorinfo.php
@@ -13,13 +13,28 @@ $uuid = $_GET["uuid"];
 
 $arrayReturn = array();
 
- $strquery = "SELECT U.username,B.locationname,M.updatetime,M.status FROM RDMONITOR M LEFT JOIN RDUSERINFO U ON M.useruuid = U.uuid LEFT JOIN RDBEACONINFO B ON M.uuid=B.UUID AND M.major=B.major AND M.minor = B.minor   WHERE M.useruuid ='" . $useruuid . "' ORDER BY M.updatetime desc LIMIT 10";
- $result = query_sql($strquery, $conn, $code, $errors);
- $index = 0;
- while ($row = fetch_single_row($result)) {
-     $index = $index + 1;
-     $arrayReturn[$row[0] . $row[2]] = array('location' => $row[1], 'updatetime' => $row[2], 'status' => $row[3]);
- }
+$strquery = "SELECT  TOP 10
+	U.username,
+	B.locationname,
+	M.updatetime,
+	M.status
+FROM
+	RDMONITOR M
+LEFT JOIN RDUSERINFO U ON M.useruuid = U.uuid
+LEFT JOIN RDBEACONINFO B ON M.uuid = B.UUID
+AND M.major = B.major
+AND M.minor = B.minor
+WHERE
+	M.useruuid = '$uuid'
+ORDER BY
+	M.updatetime DESC";
+
+$result = query_sql($strquery, $conn, $code, $errors);
+$index = 0;
+while ($row = fetch_single_row($result)) {
+    $index = $index + 1;
+    $arrayReturn[$row[0] . $row[2]] = array('location' => $row[1], 'updatetime' => $row[2], 'status' => $row[3]);
+}
 
 sendResponse(json_encode($arrayReturn));
 
