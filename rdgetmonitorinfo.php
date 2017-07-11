b006973953a6d700ba1b9867519f0c9c3c742bb4
jimiao <jimiao@hz.totyu.com>
2017-07-11 15:52:33 +0800

    adsa

--- rdgetmonitorinfo.php
+++ rdgetmonitorinfo.php
@@ -16,7 +16,7 @@ $arrayReturn = array();
 $strquery = "SELECT  TOP 10
 	U.username,
 	B.locationname,
-	M.updatetime,
+	CONVERT(VARCHAR(19),M.updatetime,120 updatetime,
 	M.status
 FROM
 	RDMONITOR M
@@ -34,7 +34,7 @@ $index = 0;
 if ($result) {
     while ($row = fetch_single_row($result)) {
         $index = $index + 1;
-        $arrayReturn[] = array('location' => $row[1], 'updatetime' => $row[2], 'status' => $row[3]);
+        $arrayReturn[$row[0] . $row[2]] = array('location' => $row[1], 'updatetime' => $row[2], 'status' => $row[3]);
     }
 }
 $arrayReturn['cd']=$code;
