c05c2f3f3fdea582e29678fda517800d3b484e25
jimiao <jimiao@hz.totyu.com>
2017-07-11 11:32:32 +0800

    adsa

--- rdupdateall.php
+++ rdupdateall.php
@@ -5,20 +5,20 @@
  * Date: 2015/06/04
  * Time: 13:57
  */
-include 'librd.php';
+include 'lib.php';
 
-//header('Content-type:text/json;charset=utf-8');
 date_default_timezone_set('Asia/Tokyo');
 
 //获取post过来的数据
-$json_string = file_get_contents('php://input'); ##今回のキモ
+$json_string = file_get_contents('php://input');
 if(ini_get("magic_quotes_gpc")=="1")
 {
     $json_string=stripslashes($json_string);
 }
 $obj = json_decode($json_string,true);
-$ret=array();
 $updatedata = $obj['updatedata'];
+
+$ret=array();
 
 foreach ($updatedata as $key => $node) {
     $useruuid = $node["useruuid"];
@@ -27,7 +27,19 @@ foreach ($updatedata as $key => $node) {
     $minor = $node["minor"];
     $status = $node["status"];
     $updatetime = $node["updatetime"];
-    $result = reupdatestatus($useruuid,$uuid,$major,$minor,$status,$updatetime);
+    //$result = reupdatestatus($useruuid,$uuid,$major,$minor,$status,$updatetime);
+
+    $sqlinsert = "INSERT INTO RDMONITOR (useruuid, uuid,major,minor,updatetime, status) VALUES ('" . $useruuid . "', '" . $uuid . "', '" . $major . "', '" . $minor . "','" . $updatetime . "','" . $status . "')";
+    $ret['sql'] = $sqlinsert;
+    $result = query_sql($sqlinsert, $conn, $code, $errors);
+
+    $sqlstsins = "INSERT INTO RDUSERSTATUS (useruuid, uuid, major,minor,updatetime) VALUES ('" . $useruuid . "', '" . $uuid . "', '" . $major . "', '" . $minor . "', '" . $updatetime . "')";
+    $sqlstsdel = "DELETE FROM RDUSERSTATUS WHERE useruuid='" . $useruuid . "' AND uuid='" . $uuid . "' AND major='" . $major . "' AND minor='" . $minor . "'";
+    if ($status == '1') {
+        $result = query_sql($sqlstsins, $conn, $code, $errors);
+    } else {
+        $result = query_sql($sqlstsdel, $conn, $code, $errors);
+    }
 }
 
 sendResponse(json_encode( $ret));
