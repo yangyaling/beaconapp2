1f7c697904552c1cc8481301567fe363f36dbb4d
jimiao <jimiao@hz.totyu.com>
2017-07-11 11:00:42 +0800

    adsa

--- rdgetlocation.php
+++ rdgetlocation.php
@@ -18,10 +18,10 @@ if ($result) {
     while ($row = fetch_single_row($result)) {
         $arrayReturn[$row[0]] = array('uuid' => $row[1], 'major' => $row[2], 'minor' => $row[3]);
     }
-    echo "t";
+    //echo "t";
 } else {
     $arrayReturn['yyy'] = sqlsrv_errors();
-    echo "f";
+   // echo "f";
 }
 //return $arrayReturn;
 
